531f7c83cf0058f56697afc92d7e53b11d88fade
AliceJain <jainalice31@gmail.com>
2023-08-13 21:35:24 +0530

    minor change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="text-3xl font-bold underline">
-        Hello World!
+        Hello World! alice jhingur
       </h1>
     </div>
   )
